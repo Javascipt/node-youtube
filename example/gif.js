28483f4eee110ece406cad32758c1d9319fcb8ea
khalid <k.rehoui@dm73.net>
2016-07-13 09:42:07 +0100

    new example for gif with size and fps

--- example/gif.js
+++ example/gif.js
@@ -9,3 +9,13 @@ rub('https://www.youtube.com/watch?v= ????')
         console.log("err : ", err)
     });
 
+
+// example with size and fps
+
+rub('https://www.youtube.com/watch?v= ????')
+    .gif('0:05', '0:35', './file.gif', '600x300', 8)
+    .then(function () {
+        console.log("Done");
+    }).catch(function (err) {
+        console.log("err : ", err)
+    });
