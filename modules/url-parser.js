3402d096d41a5b7a5dbe17faaac156d3c807ee08
Khalid REHIOUI <Rehioui@gmail.com>
2016-07-21 22:01:26 +0100

    Less opinionated url parser.

--- modules/url-parser.js
+++ modules/url-parser.js
@@ -3,10 +3,8 @@ var parser = require('url-parse');
 module.exports = function (urlOrVidId) {
     var host = parser(urlOrVidId).host;
     
-    if((host != 'youtube.com') && (host != 'youtu.be') && (host == urlOrVidId.toLowerCase())) {
+    if((host != 'youtube.com') && (host != 'youtu.be') && (host != 'www.youtube.com') && (host != 'www.youtu.be') && (host == urlOrVidId.toLowerCase())) {
         return 'https://youtu.be/' + urlOrVidId;
-    } else if((host != 'youtube.com') && (host != 'youtu.be')) {
-        throw new Error('The url specified isn\'t a Youtube video');
     }
     
     return urlOrVidId;
